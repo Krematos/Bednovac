f1f41fac8bbb2a0804abc76f80004353a164c189
Krematos <hmac1560@gmail.com>
2025-09-08 23:12:29 +0200

    upgrade designu

--- src/main/java/com/example/bednovac/CaseHarrdener.java
+++ src/main/java/com/example/bednovac/CaseHarrdener.java
@@ -6,13 +6,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 public class CaseHarrdener extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CaseHarrdener.class.getResource("/com/example/bednovac/MainView.fxml"));
+        URL fxmlUrl = CaseHarrdener.class.getResource("/com/example/bednovac/MainView.fxml");
+        System.out.println("FXML path: " + fxmlUrl);
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        //scene.getStylesheets().add(CaseHarrdener.class.getResource("/com/example/bednovac/style.css").toExternalForm());
+        System.out.println(CaseHarrdener.class.getResource("/com/example/bednovac/Style.css"));
+        scene.getStylesheets().add(Objects.requireNonNull(CaseHarrdener.class.getResource("/com/example/bednovac/Style.css")).toExternalForm());
         stage.setTitle("Case Harrdener");
         stage.setScene(scene);
         stage.show();
